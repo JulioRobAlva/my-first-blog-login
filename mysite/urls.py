d3430b0a1127d8025ad31a951c341cd76d2b7674
juliorobalva <juliorobalva@gmail.com>
2018-10-31 23:34:52 -0600

    login

---
+++ mysite/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    path('', include('blog.urls')),
+]
